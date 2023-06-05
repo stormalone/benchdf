61948f193ea5c5ed1c9830620639306c63e9f8d3
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2023-06-04 20:09:44 -0700

    initial commit of files

---
+++ src/bin/gen_insert.rs
@@ -0,0 +1,139 @@
+use clap::Parser;
+use csv;
+use std::error::Error;
+use std::fs::File;
+use std::io::{LineWriter, Write};
+use std::path::PathBuf;
+
+use arrow_schema::DataType;
+use datafusion_benchmarks::tpch::{get_tpch_table_schema, TPCH_TABLES};
+
+#[derive(clap::Parser, Debug, Clone)]
+#[clap(author, version, about, long_about = None)]
+pub struct Giconfig {
+    /// Input path
+    #[arg(value_parser, long = "input", default_value = "./dataout/")]
+    input_path: PathBuf,
+
+    /// Output path
+    #[arg(value_parser, long = "output", default_value = "./valueout")]
+    output_path: PathBuf,
+}
+
+pub const INSERT0: &str = "flight_sql_client --host 0.0.0.0 --port 50060 --username admin --password password \"insert into ";
+pub const INSERT1: &str = " values (";
+pub const INSERT2: &str = ")\"";
+
+fn main() {
+    let config = Giconfig::parse();
+
+    let _ = check_dirs(config.output_path.clone());
+
+    let x = process_values(
+        config.input_path.to_str().unwrap(),
+        config.output_path.to_str().unwrap(),
+    );
+
+    println!("{:?}", x);
+}
+
+fn check_dirs(outdir: PathBuf) -> std::io::Result<()> {
+    std::fs::create_dir(outdir)?;
+    Ok(())
+}
+
+pub fn process_values(input_path: &str, output_path: &str) -> std::io::Result<()> {
+    for table in TPCH_TABLES {
+        println!("\n{}", table);
+        let input_path = format!("{input_path}/{table}.tbl");
+        //println!("{:?}", input_path);
+
+        let output_path = format!("{output_path}");
+        //println!("{:?}", output_path);
+
+        let _ = get_vec_from_file(table, input_path.as_str(), output_path.as_str());
+    }
+    Ok(())
+}
+
+fn get_vec_from_file(
+    table_name: &str,
+    input_path: &str,
+    output_path: &str,
+) -> Result<(), Box<dyn Error>> {
+    let schema = get_tpch_table_schema(table_name);
+    println!("{:?}", schema);
+    //println!("{:?}", schema.fields[0].data_type());
+
+    let mut reader = csv::ReaderBuilder::new()
+        .delimiter(b'|')
+        .has_headers(false)
+        .from_path(input_path)?;
+
+    let output_path = format!("{output_path}/{table_name}.sh");
+    println!("{:?}", output_path);
+
+    let output_file = File::create(output_path.clone())?;
+    let mut output_file = LineWriter::new(output_file);
+
+    // `.records` return an iterator of the internal
+    // record structure
+    for result in reader.records() {
+        let record: csv::StringRecord = result?;
+        //println!("{:?}", record);
+        let record_iter = record.iter();
+        //let vec1: Vec<_> = record_iter.clone().collect();
+        let mut vec = Vec::new();
+
+        vec.push(INSERT0);
+        vec.push(table_name);
+        vec.push(INSERT1);
+
+        for (i, val) in record_iter.enumerate() {
+            if val != "" {
+                let datatype = schema.fields[i].data_type();
+                println!("{:?}", datatype);
+                let answer = should_val_be_a_string(datatype.clone());
+                // if yes then add the tickmarks around the string, date
+                if answer {
+                    vec.push("'");
+                    vec.push(val);
+                    vec.push("'");
+                    vec.push(",");
+                // if no then we do not need tick marks around the value
+                } else {
+                    vec.push(val);
+                    vec.push(",");
+                }
+                vec.push(" ");
+            }
+        }
+
+        vec.pop(); // remove the last space
+        vec.pop(); // remove the last comma
+        vec.push(INSERT2);
+        output_file.write_all(vec.concat().as_bytes())?;
+        output_file.write_all(b"\n")?;
+
+        // this prints each item of the vector on a separate line
+        /*
+        for i in vec.iter() {
+            println!("{}", i);
+        }
+        */
+        // this prints double quotes which we do not want
+        // println!("\n{:?}", vec);
+    }
+    println!("\n{:?}", output_path);
+    Ok(())
+}
+
+fn should_val_be_a_string(datatype: DataType) -> bool {
+    if datatype.equals_datatype(&DataType::Utf8) {
+        return true;
+    }
+    if datatype.equals_datatype(&DataType::Date32) {
+        return true;
+    }
+    return false;
+}
---
+++ src/bin/reduce_table.rs
@@ -0,0 +1,102 @@
+use clap::Parser;
+use std::fs::File;
+use std::io::{self, BufRead, LineWriter, Write};
+use std::path::Path;
+use std::path::PathBuf;
+
+pub const TPCH_TABLES: &[&str] = &[
+    "part", "supplier", "partsupp", "customer", "orders", "lineitem", "nation", "region",
+];
+
+#[derive(clap::Parser, Debug, Clone)]
+#[clap(author, version, about, long_about = None)]
+pub struct Rtconfig {
+    /// The number of rows in a table
+    #[arg(long, default_value_t = 20)]
+    pub row_capacity: usize,
+
+    /// Start reading the table at this line
+    #[arg(long, default_value_t = 0)]
+    pub start_row: usize,
+
+    /// Stop reading the table at this line
+    #[arg(long, default_value_t = 100)]
+    pub end_row: usize,
+
+    /// Input path
+    #[arg(value_parser, long = "input", default_value = "./data/")]
+    input_path: PathBuf,
+
+    /// Output path
+    #[arg(value_parser, long = "output", default_value = "./dataout")]
+    output_path: PathBuf,
+}
+
+fn main() {
+    // File hosts must exist in current path before this produces output
+
+    let config = Rtconfig::parse();
+    println!("{}", config.row_capacity);
+
+    let _ = check_dirs(config.output_path.clone());
+
+    let x = copy_table(
+        config.input_path.to_str().unwrap(),
+        config.output_path.to_str().unwrap(),
+        config.row_capacity,
+    );
+
+    println!("{:?}", x);
+}
+
+fn check_dirs(outdir: PathBuf) -> std::io::Result<()> {
+    std::fs::create_dir(outdir)?;
+    Ok(())
+}
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+pub fn copy_table(input_path: &str, output_path: &str, row_capacity: usize) -> std::io::Result<()> {
+    for table in TPCH_TABLES {
+        let input_path = format!("{input_path}/{table}.tbl");
+        //println!("{:?}", input_path);
+
+        let output_path = format!("{output_path}/{table}.tbl");
+        //println!("{:?}", output_path);
+
+        let output_file = File::create(output_path)?;
+        let mut output_file = LineWriter::new(output_file);
+
+        let mut counter = 0;
+
+        if let Ok(lines) = read_lines(input_path) {
+            // Consumes the iterator, returns an (Optional) String
+            for line in lines {
+                if counter >= row_capacity {
+                    break;
+                };
+                counter += 1;
+                if let Ok(ip) = line {
+                    //println!("{}", ip);
+                    output_file.write_all(ip.as_bytes())?;
+                    output_file.write_all(b"\n")?;
+                }
+            }
+        }
+    }
+    Ok(())
+}
+
+/*
+References:
+To see the number of lines in a file go to the directory and enter the command
+wc -l *
+*/
